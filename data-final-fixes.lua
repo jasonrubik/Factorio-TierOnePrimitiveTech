c0afd5ce85aa8a065e13751b0ae4fc925b8d9317
jasonrubik <jasonrubik@gmail.com>
2023-04-20 22:59:58 -0500

    Added Settings logic to remove already researched recipes
        - Added Settings logic to remove already researched recipes from Technologies which become re-enabled via settings at a later time.
        - Passive Provider Chest and Storage Chest recipes now depend on the "Allow Iron Chest" setting
        - Tried to hide fast and stack inserters from planner UI via flags on line 254 of data-final-fixes.lua

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -101,16 +101,22 @@ end
 
 if not settings.startup["TOPT-AllowConstructionRobots"].value then
 	data.raw.technology["construction-robotics"].hidden = true
+	data.raw.recipe["construction-robot"].hidden = true
 end
 
 if not settings.startup["TOPT-AllowCoalLiquefaction"].value then
 	data.raw.technology["coal-liquefaction"].hidden = true
+	data.raw.recipe["coal-liquefaction"].hidden = true
 end
 
 if not settings.startup["TOPT-AllowArtillery"].value then
 	data.raw.technology["artillery"].hidden = true
 	data.raw.technology["artillery-shell-range-1"].hidden = true
 	data.raw.technology["artillery-shell-speed-1"].hidden = true
+	data.raw.recipe["artillery-turret"].hidden = true
+	data.raw.recipe["artillery-wagon"].hidden = true
+	data.raw.recipe["artillery-shell"].hidden = true
+	data.raw.recipe["artillery-targeting-remote"].hidden = true
 end
 
 if not settings.startup["TOPT-AllowModularArmor"].value then
@@ -132,33 +138,61 @@ if not settings.startup["TOPT-AllowModularArmor"].value then
 	data.raw.technology["personal-laser-defense-equipment"].hidden = true
 	data.raw.technology["personal-roboport-equipment"].hidden = true
 	data.raw.technology["personal-roboport-mk2-equipment"].hidden = true
+	data.raw.recipe["modular-armor"].hidden = true
+	data.raw.recipe["power-armor"].hidden = true
+	data.raw.recipe["power-armor-mk2"].hidden = true
+	data.raw.recipe["effectivity-module"].hidden = true
+	data.raw.recipe["effectivity-module-2"].hidden = true
+	data.raw.recipe["fusion-reactor-equipment"].hidden = true
+	data.raw.recipe["solar-panel-equipment"].hidden = true
+	data.raw.recipe["energy-shield-equipment"].hidden = true
+	data.raw.recipe["energy-shield-mk2-equipment"].hidden = true
+	data.raw.recipe["discharge-defense-equipment"].hidden = true
+	data.raw.recipe["exoskeleton-equipment"].hidden = true
+	data.raw.recipe["belt-immunity-equipment"].hidden = true
+	data.raw.recipe["night-vision-equipment"].hidden = true
+	data.raw.recipe["battery-equipment"].hidden = true
+	data.raw.recipe["battery-mk2-equipment"].hidden = true
+	data.raw.recipe["personal-laser-defense-equipment"].hidden = true
+	data.raw.recipe["personal-roboport-equipment"].hidden = true
+	data.raw.recipe["personal-roboport-mk2-equipment"].hidden = true
 end
 
 if not settings.startup["TOPT-AllowCombatRobots"].value then
 	data.raw.technology["defender"].hidden = true
-	data.raw.technology["destroyer"].hidden = true
 	data.raw.technology["distractor"].hidden = true
+	data.raw.technology["destroyer"].hidden = true
+	data.raw.recipe["defender-capsule"].hidden = true
+	data.raw.recipe["distractor-capsule"].hidden = true
+	data.raw.recipe["destroyer-capsule"].hidden = true
 end
 
 if not settings.startup["TOPT-AllowLaserTurrets"].value then
-	data.raw.technology["energy-weapons-damage-1"].hidden = true
 	data.raw.technology["laser"].hidden = true
-	data.raw.technology["laser-shooting-speed-1"].hidden = true
 	data.raw.technology["laser-turret"].hidden = true
+	data.raw.technology["laser-shooting-speed-1"].hidden = true
+	data.raw.technology["energy-weapons-damage-1"].hidden = true
+	data.raw.recipe["laser-turret"].hidden = true
 end
 
 if not settings.startup["TOPT-AllowSpidertron"].value then
 	data.raw.technology["spidertron"].hidden = true
 	data.raw.technology["effectivity-module-3"].hidden = true
+	data.raw.recipe["spidertron"].hidden = true
+	data.raw.recipe["effectivity-module-3"].hidden = true
 end
 
 if not settings.startup["TOPT-AllowRocketLauncher"].value then
 	data.raw.technology["explosive-rocketry"].hidden = true  
 	data.raw.technology["rocketry"].hidden = true
+	data.raw.recipe["rocket"].hidden = true
+	data.raw.recipe["rocket-launcher"].hidden = true
+	data.raw.recipe["explosive-rocket"].hidden = true
 end
 
 if not settings.startup["TOPT-AllowAtomicBombs"].value then
 	data.raw.technology["atomic-bomb"].hidden = true
+	data.raw.recipe["atomic-bomb"].hidden = true
 end
 
 
@@ -181,19 +215,35 @@ end
 
 -- Update recipes for items which depend on a disabled ingredient
 
-data.raw.recipe["logistic-chest-storage"].ingredients =
-{
-      {"wooden-chest", 1},
-      {"electronic-circuit", 3},
-      {"advanced-circuit", 1}
-}
+if not settings.startup["TOPT-AllowIronChestDefaultRecipe"].value then
+	data.raw.recipe["logistic-chest-storage"].ingredients =
+		{
+			{"wooden-chest", 1},
+			{"electronic-circuit", 3},
+			{"advanced-circuit", 1}
+		}
+	data.raw.recipe["logistic-chest-passive-provider"].ingredients =
+		{
+			{"wooden-chest", 1},
+			{"electronic-circuit", 3},
+			{"advanced-circuit", 1}
+		}
+	else
+	data.raw.recipe["logistic-chest-storage"].ingredients =
+		{
+			{"iron-chest", 1},
+			{"electronic-circuit", 3},
+			{"advanced-circuit", 1}
+		}
+	data.raw.recipe["logistic-chest-passive-provider"].ingredients =
+		{
+			{"iron-chest", 1},
+			{"electronic-circuit", 3},
+			{"advanced-circuit", 1}
+		}
+end
 
-data.raw.recipe["logistic-chest-passive-provider"].ingredients =
-{
-      {"wooden-chest", 1},
-      {"electronic-circuit", 3},
-      {"advanced-circuit", 1}
-}
+
 
 --RemoveIngredientFromRecipe(data.raw.recipe["logistic-chest-storage"], "steel-chest")
 --RemoveIngredientFromRecipe(data.raw.recipe["logistic-chest-passive-provider"], "steel-chest")
@@ -201,7 +251,7 @@ data.raw.recipe["logistic-chest-passive-provider"].ingredients =
 
 -- Hide items from planner UI  -- this generated an error at runtime : 
 
--- data.raw.item["fast-inserter"].flags = { "hidden" } 
--- data.raw.item["stack-inserter"].flags = { "hidden" } 
+data.raw.inserter["fast-inserter"].flags = { "hidden" } 
+data.raw.inserter["stack-inserter"].flags = { "hidden" } 
 
 
